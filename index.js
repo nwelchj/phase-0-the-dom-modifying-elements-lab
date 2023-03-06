e97c046c99d20b136438532c006ed31e6190d2d1
Nolan Welch <bugygamer.mc@gmail.com>
2023-03-05 22:31:25 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your code here!
+const element = document.getElementById("main")
+//const elemnet2 = document.querySelector('body')
+console.log(element)
+element.remove();
+
+console.log(document);
+
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = "victory"
+newHeader.textContent = "Nolan is the champion"
+console.log(newHeader)
+
+
+
+
+console.log(document);
+
+
+
